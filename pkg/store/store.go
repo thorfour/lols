7cc6ece78021726fce35405babd0c7ee3a8ed3c7
Thor <thansen@digitalocean.com>
2018-05-25 16:38:45 -0500

    added more helper functions to store

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -9,6 +9,12 @@ import (
 	"github.com/minio/minio-go"
 )
 
+const (
+	version   = "v1"
+	ssl       = true
+	spacesURL = "" // TODO
+)
+
 var (
 	endpoint  string
 	accessKey string
@@ -21,7 +27,6 @@ func init() {
 	secretKey = os.Getenv("SPACES_SECRET")
 	endpoint = os.Getenv("SPACES_ENDPOINT")
 	name = "lols"
-	ssl := true
 }
 
 // Put takes an image url, downloads that image and uploads it to s3/spaces location and returns the new url string
@@ -38,16 +43,17 @@ func Put(url, newname string) (string, error) {
 // List returns a list of all the file names in the s3/spaces bucket
 // This function is meant to be called once on startup, and the results cached
 func List() ([]string, error) {
-	client, err := minio.New("", "", "", false)
+	client, err := minio.New(endpoint, accessKey, secretKey, ssl)
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to datastore: ", err)
+		return nil, fmt.Errorf("failed to connect to datastore: %v", err)
 	}
 
-	for o := range client.ListObjects("bucketname", "", false, nil) {
-		fmt.Println(o)
+	var objects []string
+	for o := range client.ListObjects(version, "", false, nil) {
+		objects = append(objects, o.Key)
 	}
 
-	return nil, nil
+	return objects, nil
 }
 
 // downloadImage downloads an image from a given url and uploads it to s3/spaces
@@ -70,10 +76,14 @@ func downloadImage(url, newfilename string) (string, error) {
 
 // storeImage copies data to a s3/spaces location
 func storeImage(img io.Reader, name string) (string, error) {
-	// Upload the image
-	// TODO
 	client, err := minio.New(endpoint, accessKey, secretKey, ssl)
 	if err != nil {
-		return "", fmt.Errorf("failed to connect to datastore: ", err)
+		return "", fmt.Errorf("failed to connect to datastore: %v", err)
 	}
+
+	if _, err := client.PutObject(version, name, img, -1, minio.PutObjectOptions{}); err != nil {
+		return "", fmt.Errorf("failed to store image: %v", err)
+	}
+
+	return name, nil
 }
