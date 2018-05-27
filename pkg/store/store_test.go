a0351c5487f91b38fc82f85d41628fc184f6c107
thor <github@thor-hansen.com>
2018-05-27 13:24:56 -0700

    changed to smaller test file

--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -6,8 +6,8 @@ import (
 
 func TestDownload(t *testing.T) {
 
-	testcorgi := "https://www.pets4homes.co.uk/images/breeds/50/d248d59954bb644e4437cce1758a9ce2.jpg"
-	newImage := "testcorgi.jpg"
+	testcorgi := "http://lolmergency.com/dis_gon_be_good.gif"
+	newImage := "dis.jpg"
 
 	_, errCh := Put(testcorgi, newImage)
 	for err := range errCh {
