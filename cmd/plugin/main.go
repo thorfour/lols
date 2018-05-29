be86db0bc472b7db3de8688c667e70323e02cd9a
Thor <thansen@digitalocean.com>
2018-05-29 08:45:17 -0500

    added sync to init of plugin

--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -7,6 +7,10 @@ import (
 	"github.com/thorfour/lols/pkg/lols"
 )
 
+func init() {
+	lols.Sync() // sync the initial internal cache
+}
+
 // Handler is the plugin handler
 func Handler(v url.Values) (string, error) {
 	text := strings.Split(v["text"][0], " ")
